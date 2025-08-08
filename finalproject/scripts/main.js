33f9ff38effc5e297d9027146381b46ea717187c
Codecamp-champ <aaron92coker@gmail.com>
2025-08-08 14:48:45 -0600

    fixes lighthouse

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -1,12 +1,9 @@
 import { setupNavToggle } from './nav.js';
-import { setupFooter } from './footer.js';
-import { fetchAndDisplayWeather } from './weather.js';
+import { updateFooter } from './footer.js';
+import { getWeatherData } from './weather.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupNavToggle();
-    setupFooter();
-
-    if (document.getElementById('weather-display')) {
-        fetchAndDisplayWeather();
-    }
+    updateFooter();
+    getWeatherData();
 });
